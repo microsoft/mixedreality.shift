bf5be8c834f9520dd41e4625aceec093845ad299
Chaehong Lee <chaelee@microsoft.com>
2022-09-14 09:53:52 -0700

    Add integration tests

---
+++ Shift.IntegrationTests/AdoBrokerTests.cs
@@ -0,0 +1,62 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Microsoft">
+//     Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shift.Core.Brokers;
+
+namespace Shift.IntegrationTests
+{
+    [TestClass]
+    public class AdoPackageFeedBrokerTests
+    {
+        /// <summary>
+        /// Azure devOps resource Id
+        /// </summary>
+        private static readonly string AdoResourceId = "499b84ac-1321-427f-aa17-267ca6975798";
+
+        [TestMethod]
+        public void AdoPackageFeedBroker_CanDownloadArtifactTool()
+        {
+            // arrange
+            var tokenBroker = new AdoTokenBroker();
+            var broker = new AdoPackageFeedBroker(tokenBroker, NullLogger<AdoPackageFeedBroker>.Instance);
+            var path = Path.Combine(Path.GetTempPath(), "artifacttool");
+
+            // act
+            Assert.IsTrue(new FileInfo(Path.Combine(path, "artifacttool.exe")).Exists);
+        }
+
+        [TestMethod]
+        public async Task AdoPackageFeedBroker_CanGetLatestVersion()
+        {
+            // arrange
+            var tokenBroker = new AdoTokenBroker();
+            var broker = new AdoPackageFeedBroker(tokenBroker, NullLogger<AdoPackageFeedBroker>.Instance);
+
+            // act
+            var versionString = await broker.GetLatestPackageVersionAsync(
+                feedName: "MixedReality.Build.Tools",
+                packageId: "mrshift-cli-windows",
+                organization: "https://microsoft.visualstudio.com/",
+                project: "Analog");
+
+            // assert
+            Assert.IsNotNull(versionString);
+            var version = Version.Parse(versionString);
+            Assert.IsTrue(version > new Version(0, 0, 1));
+        }
+
+        protected async Task<string> GetPatAsync()
+        {
+            var tokenCredential = new DefaultAzureCredential();
+            var authResult = await tokenCredential.GetTokenAsync(new TokenRequestContext(scopes: new string[] { AdoResourceId + "/.default" }) { });
+            return authResult.Token;
+        }
+    }
+}
---
+++ Shift.IntegrationTests/AdoTokenBrokerTests.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Microsoft">
+//     Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shift.Core.Brokers;
+
+namespace Shift.IntegrationTests
+{
+    [TestClass]
+    public class AdoTokenBrokerTests
+    {
+        [TestMethod]
+        public async Task AdoTokenBroker_CanGetToken_WithOrganizationId()
+        {
+            // arrange
+            var broker = new AdoTokenBroker();
+
+            // act
+            var token = await broker.GetTokenCredentialAsync("microsoft");
+
+            // assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(token));
+        }
+
+        [TestMethod]
+        public async Task AdoTokenBroker_CanGetToken_WithOrganizationId_FromEnvVar()
+        {
+            // arrange
+            var broker = new AdoTokenBroker();
+
+            // act
+            Environment.SetEnvironmentVariable("AZURE_DEVOPS_EXT_PAT_MICROSOFT", "abc");
+            var token = await broker.GetTokenCredentialAsync("microsoft");
+
+            // assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(token));
+            Assert.AreEqual("abc", token);
+        }
+    }
+}
