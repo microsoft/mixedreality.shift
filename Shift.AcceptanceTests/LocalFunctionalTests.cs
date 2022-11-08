4277cd75700dcf4a48b4a555be2f24eaa41adb83
Chaehong Lee <chaelee@microsoft.com>
2022-11-08 14:01:30 -0800

    Skip copying artifacts in folder if already exists

--- Shift.AcceptanceTests/LocalFunctionalTests.cs
+++ Shift.AcceptanceTests/LocalFunctionalTests.cs
@@ -19,7 +19,7 @@ namespace Shift.Cli.AcceptanceTests
     /// </summary>
     [TestClass]
     [TestCategory("LOCAL-TESTS")]
-    public class LocalFunctionalTests
+    public partial class LocalFunctionalTests
     {
         private static string _filepath;
 
@@ -71,116 +71,6 @@ namespace Shift.Cli.AcceptanceTests
             // assert
             Assert.AreEqual(0, process.ExitCode);
             Assert.IsTrue(output.Contains("1.0.0"));
-        }
-
-        [TestMethod]
-        public async Task Shift_CanRunManifest_WithHelloWorldSample()
-        {
-            // arrange
-            var process = CreateProcess("run \"./Data/hello-world-manifest.json\"");
-
-            // act
-            process.Start();
-            await process.WaitForExitAsync();
-            var output = await process.StandardOutput.ReadToEndAsync();
-            var error = await process.StandardError.ReadToEndAsync();
-
-            // write to output, for posterity
-            await Console.Out.WriteLineAsync(output);
-            await Console.Error.WriteLineAsync(error);
-
-            // assert
-            Assert.AreEqual(0, process.ExitCode);
-        }
-
-        [TestMethod]
-        public async Task Shift_CanRunManifest_WithBundleOptionAndHelloWorldSample()
-        {
-            // arrange
-            var process = CreateProcess("run \"./Data/hello-world-manifest.json\" --bundle supplemental");
-
-            // act
-            process.Start();
-            await process.WaitForExitAsync();
-            var output = await process.StandardOutput.ReadToEndAsync();
-            var error = await process.StandardError.ReadToEndAsync();
-
-            // write to output, for posterity
-            await Console.Out.WriteLineAsync(output);
-            await Console.Error.WriteLineAsync(error);
-
-            // assert
-            Assert.AreEqual(0, process.ExitCode);
-            Assert.IsTrue(File.Exists(Path.Combine(Path.Combine(ProgramDataPath.GetStagingDirectory(), "hello-world-2"), "readme.md")));
-        }
-
-        [TestMethod]
-        public async Task Shift_CanDownloadManifest_WithHelloWorldSample()
-        {
-            // arrange
-            var stagingDirectory = Path.Combine(Path.GetTempPath(), nameof(Shift_CanDownloadManifest_WithHelloWorldSample));
-            var process = CreateProcess($"run \"./Data/hello-world-manifest.json\" --download-only --staging-directory {stagingDirectory}");
-
-            // act
-            process.Start();
-            await process.WaitForExitAsync();
-            var output = await process.StandardOutput.ReadToEndAsync();
-            var error = await process.StandardError.ReadToEndAsync();
-
-            // write to output, for posterity
-            await Console.Out.WriteLineAsync(output);
-            await Console.Error.WriteLineAsync(error);
-
-            // assert
-            Assert.AreEqual(0, process.ExitCode);
-            Assert.IsTrue(File.Exists(Path.Combine(Path.Combine(stagingDirectory, "hello-world"), "readme.md")));
-            Assert.IsTrue(File.Exists(Path.Combine(Path.Combine(stagingDirectory, "hello-world-2"), "readme.md")));
-        }
-
-        [TestMethod]
-        public async Task Shift_CanRunManifest_WithPathToArchiveHelloWorldSample()
-        {
-            // arrange
-            var process = CreateProcess($"run \"./Data/archive.zip\"");
-
-            // act
-            process.Start();
-            await process.WaitForExitAsync();
-            var output = await process.StandardOutput.ReadToEndAsync();
-            var error = await process.StandardError.ReadToEndAsync();
-
-            // write to output, for posterity
-            await Console.Out.WriteLineAsync(output);
-            await Console.Error.WriteLineAsync(error);
-
-            // assert
-            Assert.AreEqual(0, process.ExitCode);
-            Assert.IsTrue(File.Exists(Path.Combine(Path.Combine("./Data/archive", "hello-world"), "readme.md")));
-
-            // cleanup
-            Directory.Delete("./Data/archive", recursive: true);
-        }
-
-        [TestMethod]
-        public async Task Shift_CanPack_WithHelloWorldSample()
-        {
-            // arrange
-            var outputPath = Path.GetTempFileName() + ".zip";
-            var process = CreateProcess($"pack --manifest-path \"./Data/hello-world-manifest.json\" --output-path \"{outputPath}\"");
-
-            // act
-            process.Start();
-            await process.WaitForExitAsync();
-            var output = await process.StandardOutput.ReadToEndAsync();
-            var error = await process.StandardError.ReadToEndAsync();
-
-            // write to output, for posterity
-            await Console.Out.WriteLineAsync(output);
-            await Console.Error.WriteLineAsync(error);
-
-            // assert
-            Assert.AreEqual(0, process.ExitCode);
-            Assert.IsTrue(File.Exists(outputPath));
         }
 
         private static Process CreateProcess(string args)
